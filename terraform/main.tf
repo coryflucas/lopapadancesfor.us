2790bea2e3a864f9716cdd7fe136d5b0f7c45235
Cory Lucas <cory@corylucas.com>
2025-07-16 14:11:34 -0400

    initial terraform

---
+++ terraform/main.tf
@@ -0,0 +1,32 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 6.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_s3_bucket" "bucket" {
+  bucket = "lopapadancesfor.us"
+
+  tags = {
+    project = "lopapadancesfor.us"
+  }
+}
+
+resource "aws_s3_bucket_website_configuration" "bucket" {
+  bucket = aws_s3_bucket.bucket.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
+}
